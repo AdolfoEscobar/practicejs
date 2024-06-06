0a512921b25cb9d0b73dd27b25487f40f0020296
AdolfoEscobar <adolfoescobarpe@gmail.com>
2024-06-06 14:24:13 -0600

    playing with js functions

--- playing.js
+++ playing.js
@@ -296,3 +296,101 @@ if(runnerAge > 18 && runnerEarly === true ){
 }else{
   console.log('See the registration desk');
 }
+
+//Function Declarations
+
+function getReminder(){
+  console.log('Water the plants');
+}
+function greetInSpanish(){
+  console.log('Buenas tardes');
+}
+
+//Calling a function
+
+function sayThanks(){
+  console.log('Thank you for your purchase! We appreciate your business.');
+}
+sayThanks();
+sayThanks();
+sayThanks();
+
+//Parameters
+
+function sayThanks(name) {
+  console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
+}
+
+sayThanks('Cole');
+
+//Default Parameters
+
+function makeShoppingList(item1 ='milk', item2='bread', item3='eggs'){
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+}
+
+makeShoppingList();
+
+//Return
+
+function monitorCount(rows, columns){
+  return rows * columns;
+}
+const numOfMonitors = monitorCount(5,4);
+
+console.log(numOfMonitors);
+
+//Helper functions
+
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+
+function costOfMonitors(rows, columns){
+  return monitorCount(rows, columns) * 200;
+}
+
+const totalCost = costOfMonitors(5,4);
+
+console.log(totalCost);
+
+//Function Expressions
+
+const plantNeedsWater = function(day){
+  if(day === 'Wednesday'){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+plantNeedsWater('Tuesday');
+
+console.log(plantNeedsWater('Tuesday'));
+
+//Arrow functions
+
+/*
+Arrow functions remove the need to type out the keyword function 
+every time you need to create a function. 
+Instead, you first include the parameters inside the ( ) 
+and then add an arrow => that points to the function body 
+surrounded in { } like this:
+*/
+
+const plantsNeedsWater = (day) => {
+  if (day === 'Wednesday') {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+//Concise Body Arrow Functions
+
+const plantsNeedWater = day => day === 'Wednesday' ? true : false;
+
+console.log(plantsNeedWater('Wednesday'));
