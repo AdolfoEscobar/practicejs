7d0ba5ce1f7ba10101262a89e6a9e0e488581458
AdolfoEscobar <adolfoescobarpe@gmail.com>
2024-06-11 22:49:39 -0600

    playing with js loops

--- playing.js
+++ playing.js
@@ -578,3 +578,79 @@ console.log(target0);
 console.log(target1);
 console.log(target2);
 console.log(targetnumber5);
+
+//Loops
+
+var vacationSpots = ['Colombia','Alemania','España'];
+
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+//The For Loop
+
+for(let i = 5; i <= 10; i++){
+  console.log(i);
+}
+
+//Looping in Reverse
+
+// The loop below loops from 0 to 3. Edit it to loop backwards from 3 to 0
+for (let counter = 3; counter >= 0; counter--){
+  console.log(counter);
+}
+
+//Looping through Arrays
+
+for(let i = 0; i < vacationSpots.length; i++){
+  console.log(`I would love to visit ${vacationSpots[i]}`);
+}
+
+//Nested Loops
+
+var bobsFollowers = ['Elisa','Pedro','Juan','Ashley'];
+var tinasFollowers = ['Pedro','Elisa','David'];
+var mutualFollowers = [];
+for (let i = 0; i < bobsFollowers.length; i++){
+  for(let j = 0; j < tinasFollowers.length; j++){
+    if(bobsFollowers[i] === tinasFollowers[j]){
+      mutualFollowers.push(bobsFollowers[i]);
+    }
+  }
+}
+
+console.log(mutualFollowers)
+
+//The While Loop
+
+const cards = ['diamond', 'spade', 'heart', 'club'];
+
+let currentCard = 'diamond'
+console.log(currentCard);
+while(currentCard!='spade'){
+  currentCard = cards[Math.floor(Math.random() * 4)];
+  console.log(currentCard);
+}
+
+//Do...While Statements
+
+var cupsOfSugarNeeded = 2;
+var cupsAdded = 0;
+
+do{
+  cupsAdded++
+}while(cupsAdded < cupsOfSugarNeeded);
+
+console.log(cupsAdded);
+
+//The break Keyword
+
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+for(let i = 0; i < rapperArray.length; i++){
+  console.log(rapperArray[i]);
+  if (rapperArray[i] === 'Notorious B.I.G.'){
+    break;
+  }
+}
+console.log("And if you don't know, now you know.");
