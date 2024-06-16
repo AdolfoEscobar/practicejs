60f705a1c363af635305ff72f9bbad965bc49905
AdolfoEscobar <adolfoescobarpe@gmail.com>
2024-06-16 03:15:24 -0600

    js fx iterators

--- playing.js
+++ playing.js
@@ -658,3 +658,185 @@ console.log("And if you don't know, now you know.");
 //ITERATORS
 
 //HIGHER-ORDER FUNCTIONS
+
+
+// Functions as Data
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+  for(let i = 1; i <= 1000; i++) {
+    if ( (2 + 2) != 4) {
+      console.log('Something has gone very wrong :( ');
+    }
+  }
+};
+
+const isTwoPlusTwo = checkThatTwoPlusTwoEqualsFourAMillionTimes;
+
+isTwoPlusTwo();
+console.log(isTwoPlusTwo.name);
+
+// Functions as Parameters
+
+const addTwo = num => {
+  return num + 2;
+}
+
+const checkConsistentOutput = (func, val) => {
+  var checkA = val + 2;
+  var checkB = func(val);
+  if(checkA === checkB){
+    return func(val)
+  }else{
+    return 'inconsistent results';
+  }
+}
+
+console.log(checkConsistentOutput(addTwo, 2));
+
+//Introduction to Iterators
+
+const artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
+
+artists.forEach(artist => {
+  console.log(artist + ' is one of my favorite artists.');
+});
+
+const numbers = [1, 2, 3, 4, 5];
+
+const squareNumbers = numbers.map(number => {
+  return number * number;
+});
+
+console.log(squareNumbers);
+
+const things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
+
+const onlyNumbers = things.filter(thing => {
+  return typeof thing === 'number';
+});
+
+console.log(onlyNumbers);
+
+//The .forEach() Method
+
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+fruits.forEach(fruit =>{
+  console.log(`I want to eat a ${fruit}`);
+})
+
+
+//The .map() Method
+
+//.map() works in a similar manner to .forEach()
+//the major difference is that .map() returns a new array.
+
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+const secretMessage = animals.map(animal => animal[0]);
+
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+
+const smallNumbers = bigNumbers.map(bigNumber => bigNumber/100);
+
+console.log(smallNumbers)
+
+//The .filter() Method
+
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+
+const smallsNumbers = randomNumbers.filter(num => {
+  return num < 250
+});
+
+console.log(smallsNumbers);
+
+
+const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+
+
+// Call .filter() on favoriteWords below
+
+const longFavoriteWords = favoriteWords.filter(word => {
+  return word.length > 7;
+})
+
+console.log(longFavoriteWords);
+
+
+//The .findIndex() Method
+
+const animales = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+
+const foundAnimal = animales.findIndex(animal => {
+  return animal === 'elephant';
+});
+
+console.log(foundAnimal);
+
+const startsWithS = animales.findIndex(animal => {
+  return animal[0] === 's';
+})
+
+console.log(startsWithS);
+
+
+//The .reduce() Method
+
+const newNumbers = [1, 3, 5, 7];
+
+const newSum = newNumbers.reduce((accumulator, currentValue) =>{
+  console.log('The value of accumulator: ', accumulator);
+  console.log('The value of currentValue: ', currentValue);
+  return accumulator + currentValue;
+},10);
+console.log(newSum);
+
+//Iterator Documentation
+
+const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+
+console.log(words.some((word) => {
+  return word.length < 6;
+}));
+
+const interestingWords = words.filter((word) =>{
+  return word.length > 5
+});
+
+
+console.log(interestingWords.every((word) => {
+  return word.length > 5;
+}));
+
+
+//Choose the Right Iterator
+
+const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
+
+const nums = [1, 50, 75, 200, 350, 525, 1000];
+
+//  Choose a method that will return undefined
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
+
+// Choose a method that will return a new array
+const longCities = cities.filter(city => city.length > 7);
+
+// Choose a method that will return a single value
+const word = cities.reduce((acc, currVal) => {
+  return acc + currVal[0]
+}, "C");
+
+console.log(word)
+
+// Choose a method that will return a new array
+const smallerNums = nums.map(num => num - 5);
+
+// Choose a method that will return a boolean value
+nums.every(num => num < 0);
